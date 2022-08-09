4d0d0bab022cbd8c51ec36ce337af0f3cdd7e875
Roman <roman.markanych@gmail.com>
2022-08-09 23:30:04 +0300

    add 1-task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = galleryItems.reduce(
+  (markup, { preview, original, description }) =>
+    markup +
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+  ""
+);
+gallery.addEventListener("click", onModalOpen);
+
+function onModalOpen(e) {
+  e.preventDefault();
+
+  document.addEventListener("keydown", onModalClose);
+
+  function onModalClose(e) {
+    if (e.code !== "Escape") return;
+    else {
+      instance.close();
+      document.removeEventListener("keydown", onModalClose);
+    }
+  }
+
+  const modalOpendImage = e.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${modalOpendImage}" width="700" height="500">`
+  );
+
+  instance.show();
+}

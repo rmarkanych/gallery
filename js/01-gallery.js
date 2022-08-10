8edf7e8f1247689c44ec76ae2495aa68e0249795
Roman <roman.markanych@gmail.com>
2022-08-10 21:19:56 +0300

    fixed tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,10 +2,14 @@ import { galleryItems } from "./gallery-items.js";
 
 const gallery = document.querySelector(".gallery");
 
-gallery.innerHTML = galleryItems.reduce(
-  (markup, { preview, original, description }) =>
-    markup +
-    `<div class="gallery__item">
+gallery.addEventListener("click", onModalOpen);
+document.addEventListener("keydown", onModalClose);
+
+function renderMarkup(array) {
+  gallery.innerHTML = array.reduce(
+    (markup, { preview, original, description }) =>
+      markup +
+      `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -15,27 +19,29 @@ gallery.innerHTML = galleryItems.reduce(
     />
   </a>
 </div>`,
-  ""
-);
-gallery.addEventListener("click", onModalOpen);
+    ""
+  );
+}
+
+renderMarkup(galleryItems);
 
 function onModalOpen(e) {
   e.preventDefault();
 
-  document.addEventListener("keydown", onModalClose);
-
-  function onModalClose(e) {
-    if (e.code !== "Escape") return;
-    else {
-      instance.close();
-      document.removeEventListener("keydown", onModalClose);
-    }
-  }
+  if (e.target.nodeName !== "IMG") return;
 
   const modalOpendImage = e.target.dataset.source;
   const instance = basicLightbox.create(
-    `<img src="${modalOpendImage}" width="700" height="500">`
+    `<img src="${modalOpendImage}" width="800" height="600">`
   );
 
   instance.show();
+}
+
+function onModalClose(e) {
+  if (e.code !== "Escape") return;
+  else {
+    instance.close();
+    document.removeEventListener("keydown", onModalClose);
+  }
 }
